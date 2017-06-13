8961be81cc94f913b1f145495e7a585833965d16
conan <aconanlai@gmail.com>
2017-06-13 20:33:26 +0200

    animation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ canvas.width = width;
 
 var center = width / 2;
 
-ctx.lineWidth = 30;
+ctx.lineWidth = 25;
 ctx.beginPath();
 
 ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
@@ -17,16 +17,24 @@ function clear() {
 }
 
 var outer = 150;
+var blink = true;
 
 function animate() {
+  clear();
+  var start = blink ? 50 : 75;
   var current = 0;
+  ctx.beginPath();
   while (current <= outer) {
+    ctx.arc(center, height, start + current, 0, 2 * Math.PI);
     current += 50;
-    ctx.arc(center, height, current, 0, 2 * Math.PI);
   }
   ctx.stroke();
+  ctx.closePath();
   outer += 50;
-  setTimeout(animate, 100);
+  blink = !blink;
+  if (outer <= height) {
+    setTimeout(animate, 100);
+  }
 }
 
 animate();
